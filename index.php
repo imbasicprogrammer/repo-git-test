bfe092f2ee0183c10df1785e135f7bdb27dd1faa
habib <mhabibmhabib05@gmail.com>
2024-07-14 16:02:20 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem nam beatae odio expedita id illo! Itaque animi aspernatur iusto. Praesentium pariatur consequatur sit nesciunt, eum tempore culpa quae accusamus?</p>
 </body>
 </html>
